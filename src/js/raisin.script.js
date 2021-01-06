ad3b9210e2b89488ab72010a71c0f28a20ab0aca
Simon Buechi <simon.buechi@gmail.com>
2021-01-06 09:17:56 +0100

    setup subtheme from Radix

---
+++ src/js/raisin.script.js
@@ -0,0 +1,12 @@
+/*
+(function () {
+  'use strict';
+
+  Drupal.behaviors.helloWorld = {
+    attach: function (context) {
+     // console.log('Hello, this is Radix Material');
+    }
+  };
+
+})(jQuery, Drupal);
+*/
